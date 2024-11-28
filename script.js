2b54e4b087d7612989a104e91e96f6e2f5bcc438
Rifat654 <141209933+Rifat654@users.noreply.github.com>
2024-11-28 15:11:52 +0600

    added

---
+++ script.js
@@ -0,0 +1,75 @@
+// ================ Quiz Data ====================//
+const quizData = [
+  {
+    question: "What does HTML stand for?",
+
+    Options: [
+      "Hypertext Markup Language",
+      "Hyper transfer Mack Language",
+      "Hypertext mac Language",
+      "Hypertext Markup land",
+    ],
+    correct: 0,
+  },
+
+  {
+    question: "Which CSS property used for change Font Style",
+
+    Options: ["Margin", "Display", "Font Family", "Padding"],
+    correct: 2,
+  },
+
+  {
+    question: "What is JavaScript DOM full form?",
+    Options: [
+      "Document Object Model",
+      "Document Organize Machine",
+      "Doctype Oriented Mark ",
+      "Document Object Mouse",
+    ],
+
+    correct: 0,
+  },
+
+  {
+    question: "What is the full from of WWW?",
+    Options: [
+      "Work Run Work",
+      "World Wild Water",
+      "Wild woks mark",
+      "World Wide Web",
+    ],
+
+    correct: 3,
+  },
+
+  {
+    question: "What is the symble to select css class",
+    Options: [".", "#", "+", "%"],
+    correct: 0,
+  },
+];
+
+//============ JavaScript Initialization ==============//
+
+const answerElement = document.querySelectorAll(".answer");
+
+const option = document.querySelectorAll(
+  "#question, .option_1, .option_2, .option_3, .option_4  "
+);
+
+const submit = document.querySelector("#submit");
+
+const current_quiz = 0;
+const score = 0;
+
+//============= load quiz function ================//
+
+const load = () => {
+  const { question, option } = quizData[current_quiz];
+  console.log(question);
+
+  option.innerText = question;
+};
+
+load();
